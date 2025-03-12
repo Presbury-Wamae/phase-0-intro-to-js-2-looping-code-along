857654e3a2c148b289e81f67109fcd8fd881dd1b
Presbury <wamaepresbury034@gmail.com>
2025-03-12 20:15:36 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts){
+//     for(let i=0; i < gifts.length; i++){
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`)
+//         debugger;
+//     }
+//     return gifts;
+// }
+// wrapGifts(gifts);
+
+
+// let names = ["Enan", "Wanja", "Wamae"]
+
+// function writeCards(names, thankyoucards){
+//     for(let i=0; i < names.length; i++){
+//         console.log(`Thank you so much, ${names[i]} for the gifts, they were all well wrapped!!!`)
+//     }
+// }
+// writeCards(names);
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number--);
+    }  
+}
+countDown(20);
+
